2e7285743ac9a8d34fb2b2b349a7b4867929b019
Abigail Paull <abigailpaul790@gmail.com>
2025-07-29 16:47:58 +0100

    Added Naria Exchange rate

--- src/Component/CurrencySelect.jsx
+++ src/Component/CurrencySelect.jsx
@@ -1,4 +1,4 @@
-const currencyCodes =[ "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
+const currencyCodes =[ "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "NGN",
     "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL",
     "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY",
     "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP",
